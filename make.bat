3bb3dbb86d7a5dca42b0591bc229911accc2eaca
dalex <dalex5942@gmail.com>
2024-02-16 14:13:04 +1000

    Updated make.bat.

--- make.bat
+++ make.bat
@@ -2,7 +2,7 @@
 cls
 pushd bin
 
-cl.exe ..\src\main.cpp ..\src\confparse.cpp -DIC_ALL -std:c++20 -Zi -permissive -link -nologo -incremental:no kernel32.lib shell32.lib user32.lib -out:test.exe
+cl.exe ..\src\main.cpp -DCFGPARSE_IMPLEMENTATION -DCFGPARSE_ALL -std:c++20 -Zi -permissive -link -nologo -incremental:no kernel32.lib shell32.lib user32.lib -out:test.exe
 copy test.exe ..
 
 popd
